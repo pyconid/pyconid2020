73168f32b430f9f65c3ac2cccc577904237479b8
adifdwimaulana <adifdwimaulana@gmail.com>
2020-08-29 21:21:11 +0700

    Pull and update from .gitignore

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://pycon.id'
+# SITEURL = 'https://pycon.id'
+SITEURL = ''
 
 RELATIVE_URLS = False
 
